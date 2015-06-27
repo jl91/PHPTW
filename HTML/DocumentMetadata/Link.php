79651e78e1411a94494796d476ff6295c7f1515f
john lennon de melo fernandes <johnlennon@progiro.com.br>
2015-06-27 03:16:20 -0300

    add link Tag Class

---
+++ HTML/DocumentMetadata/Link.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace HTML\DocumentMetadata;
+
+/**
+ * Description of Head
+ *
+ * @author desenvolvimento-01
+ */
+use HTML\Element\AbstractSelfCloseTag;
+
+class Link extends AbstractSelfCloseTag
+{
+    protected $tag              = 'link';
+    protected $isSelfClosingTag = true;
+
+}
--- main.php
+++ main.php
@@ -6,18 +6,32 @@ ini_set('display_errors', true);
 
 use HTML;
 
-$html  = new HTML\Root\HTML();
-$head  = new HTML\DocumentMetadata\Head();
-$head->addAttribute('data-teste', 'teste');
-$title = (new HTML\DocumentMetadata\Title())
-    ->addContent("Título");
-$head
-    ->addElement($title);
+//HTML Tag
+$html = new HTML\Root\HTML();
 
+//Head Tag
+$head = new HTML\DocumentMetadata\Head();
+$head->addAttribute('data-teste', 'teste');
+
+//Title Tag
+$title = new HTML\DocumentMetadata\Title();
+$title->addContent("Título");
+
+
+//Base Tag
 $base = new HTML\DocumentMetadata\Base();
 
-$html->addElement($head)
-    ->addElement($base);
+//Link Tag
+$link = new HTML\DocumentMetadata\Link();
+$link->addAttribute('type', 'text/css')
+    ->addAttribute('rel', 'stylesheet');
+
+
+$head->addElement($base)
+    ->addElement($link);
+
+
+$html->addElement($head);
 
 $render = new HTML\Render\DefaultElementRender();
 $render->render($html);
