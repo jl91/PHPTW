f34a2d471f4c5e72d2ec5e6fdaee31295504335d
jl91 <johnlennon@progiro.com.br>
2015-08-19 00:15:16 -0300

    Add Embedded content Package and classes

---
+++ HTML/EmbeddedContent/Area.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace HTML\EmbeddedContent;
+
+/**
+ * Description of Head
+ *
+ * @author desenvolvimento-01
+ */
+use \Html\Element\AbstractElement;
+
+class Area extends AbstractElement
+{
+    protected $tag = 'area';
+    protected $isSelfClosingTag = true;
+    
+
+}
---
+++ HTML/EmbeddedContent/Track.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace HTML\EmbeddedContent;
+
+/**
+ * Description of Head
+ *
+ * @author desenvolvimento-01
+ */
+use \Html\Element\AbstractElement;
+
+class Track extends AbstractElement
+{
+    protected $tag = 'track';
+    protected $isSelfClosingTag = true;
+    
+
+}
