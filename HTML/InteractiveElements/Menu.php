75aa989e76f4681454a2a70512d4df01a0ae98bd
jl91 <johnlennon@progiro.com.br>
2015-08-19 00:49:00 -0300

    Add Interactive Elements Package and classes

---
+++ HTML/InteractiveElements/Menu.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace HTML\InteractiveElements;
+
+/**
+ * Description of Head
+ *
+ * @author desenvolvimento-01
+ */
+use HTML\Element\AbstractSelfCloseTag;
+
+class Menu extends AbstractSelfCloseTag
+{
+    protected $tag = 'menu';
+
+}
